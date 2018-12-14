fd30c17dc20b6f1ec9d2e5c5fed4781bf17874ba
Abraham Alex <a.abrahamalex@hotmail.com>
2018-12-13 21:55:29 -0500

    abc

--- build_site.R
+++ build_site.R
@@ -2,7 +2,7 @@
 #This helps avoid confusion if our working directory is 
 #not our site because of other projects we were 
 #working on at the time. 
-setwd("/Users/abrahamalex/Desktop/Psychology-Statistics")
+setwd("/Users/abrahamalex/Psychology-Statistics")
 
 #render your sweet site. 
 rmarkdown::render_site()
